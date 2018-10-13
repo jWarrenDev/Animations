5952b16710f6b17dac13bce9cf4b54aa8c41d3d3
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-13 16:58:58 -0700

    storyboard Animate button

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -13,11 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // dont need to use transient because we disabled autoLayout
         let greenSquare = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         greenSquare.backgroundColor = .green
         view.addSubview(greenSquare)
     }
 
-
+    @IBAction func animate(_ sender: Any) {
+        
+    }
+    
 }
 
