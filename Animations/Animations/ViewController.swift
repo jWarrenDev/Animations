ceeae4b8b8007cecd6ebc76ef2e19d0dc8c652c6
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-13 17:03:58 -0700

    created frame and x / y origin instructions

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -12,16 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // dont need to use transient because we disabled autoLayout
-        let greenSquare = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         greenSquare.backgroundColor = .green
         view.addSubview(greenSquare)
     }
 
     @IBAction func animate(_ sender: Any) {
-        
+        UIView.animate(withDuration: 0.5) {
+            // create frame for greenSquare
+            var frame = self.greenSquare.frame
+            frame.origin.x += 25
+            frame.origin.y += 75
+            
+            self.greenSquare.frame = frame
+        }
     }
     
+     // dont need to use transient because we disabled autoLayout
+     let greenSquare = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
 }
 
